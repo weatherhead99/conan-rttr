56bff408887aac98a6c786de5a4e804314457028
Dan Weatherill <plasteredparrot@gmail.com>
2018-02-03 22:07:09 +0000

    add patch to build static lib properly, and generate cmake config files properly

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,6 @@
 from conans import ConanFile, CMake, tools
 import os
+import shutil
 
 class RttrConan(ConanFile):
     name = "rttr"
@@ -9,38 +10,36 @@ class RttrConan(ConanFile):
     description = "<Description of Rttr here>"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False],
-               "rtti" : [True, False]}
+               "rtti": [True, False]}
     default_options = "shared=False", "rtti=True"
     generators = "cmake"
-    requires = "boost_chrono/1.66.0@bincrafters/testing","boost_system/1.66.0@bincrafters/testing"
-    exports_sources = "CMakeLists.txt"
     exports = "README.md"
-    
+    exports_sources = "LICENSE.txt", "conan_build.patch"
     sha256 = "caa8d404840b0e156f869a947e475b09f7b602ab53c290271f40ce028c8d7d91"
 
 
     def source(self):
         tools.get("http://www.rttr.org/releases/rttr-%s-src.tar.gz"
                   % self.version, sha256=self.sha256)
-        sf = os.path.join(self.source_folder,"rttr-%s-src" % self.version)
-        os.rename(sf,"source_subfolder")
+        tools.patch(patch_file="conan_build.patch")
         
     def build(self):
         cmake = CMake(self)
         cmake.definitions["BUILD_BENCHMARKS"] = "OFF"
         cmake.definitions["BUILD_WITH_RTTI"] = "ON" if self.options.rtti else "OFF"
-
-        cmake.configure()
+        cmake.definitions["BUILD_STATIC"] = "ON" if not self.options.shared else "OFF"
+        sf = os.path.join(self.source_folder,"rttr-%s-src" % self.version)
+        cmake.configure(source_folder=sf)
         cmake.build()
+        #need to do install here, otherwise cmake config files don't get
+        #generated properly
         cmake.install()
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        pass
 
+        
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["rttr_core"]
+
+
