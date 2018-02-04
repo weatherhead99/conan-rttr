a009b4b8862097558bb2f87c6ea16698259ed5c3
Dan Weatherill <plasteredparrot@gmail.com>
2018-02-04 02:11:57 +0000

    fix test to work on MSVC

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -12,11 +12,17 @@ int main(int argc, char** argv)
 
      auto type = rttr::type::get<dblvec>();
     std::cout << "type name: " << type.get_name() << std::endl;;
-    if(type.get_name() != "std::vector<double>")
+
+    dblvec vec(0.0);
+
+    auto type2 = rttr::type::get(vec);
+
+    if(type.get_name() != type2.get_name())
       {
-	std::cout << "test failed! " << std::endl;
+	std::cout << "test failed!" << std::endl;
 	return EXIT_FAILURE;
       }
+	
     std::cout << "test passed!" << std::endl;
     return EXIT_SUCCESS;
 
